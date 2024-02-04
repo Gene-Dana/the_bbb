e5344ced6951d7182b2681c2a8d1bd9e3d6fc08a
Gene <geneyllanes@gmail.com>
2024-02-03 22:40:53 -0500

    client logo section

--- lib/client_logos.dart
+++ lib/client_logos.dart
@@ -5,6 +5,37 @@ class ClientLogos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        height: 300,
+        color: Color.fromARGB(255, 36, 73, 222),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('logo1.png'),
+                  Image.asset('logo8.png'),
+                  Image.asset('logo2.png'),
+                  Image.asset('logo4.png'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('logo5.png'),
+                  Image.asset('logo3.png'),
+                  Image.asset('logo6.png'),
+                  Image.asset('logo7.png'),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
