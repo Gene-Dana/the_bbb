cb7495df40c30d585c7dc9359b11b21a26d7d2c8
Gene <geneyllanes@gmail.com>
2024-02-03 22:18:13 -0500

    updates

--- lib/opening.dart
+++ lib/opening.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:the_bbb/btns.dart';
-import 'package:the_bbb/main.dart';
 
 class Opening extends StatelessWidget {
   const Opening({super.key});
@@ -10,46 +9,54 @@ class Opening extends StatelessWidget {
     return Container(
         height: MediaQuery.of(context).size.height - 170,
         color: const Color.fromRGBO(1, 15, 48, 1),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 128.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Text('Saving you time for what you do best',
-                    style: Theme.of(context).textTheme.displayLarge),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: SizedBox(
-                  width: 850,
-                  child: Text(
-                    'Are you a small business owner looking for hassle-free bookkeeping services without the burden of hiring an in-house employee? Look no further! Our dedicated team is here to streamline your financial processes, so you can focus on what you do best – growing your business.',
-                    style: Theme.of(context).textTheme.bodySmall,
+        child: Stack(
+          children: [
+            Positioned.fill(
+                top: 300.0,
+                right: -800.0,
+                child: Image.asset('assets/abstract.png')),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 128.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Text('Saving you time for what you do best',
+                        style: Theme.of(context).textTheme.displayLarge),
                   ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(24.0),
-                child: SizedBox(
-                  width: 700,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: MainBtn(title: 'Our Services', link: ''),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: SizedBox(
+                      width: 850,
+                      child: Text(
+                        'Are you a small business owner looking for hassle-free bookkeeping services without the burden of hiring an in-house employee? Look no further! Our dedicated team is here to streamline your financial processes, so you can focus on what you do best – growing your business.',
+                        style: Theme.of(context).textTheme.bodySmall,
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: SecondaryBtn(title: 'Contact Us', link: ''),
-                      )
-                    ],
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
+                  const Padding(
+                    padding: EdgeInsets.all(24.0),
+                    child: SizedBox(
+                      width: 700,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: MainBtn(title: 'Our Services', link: ''),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: SecondaryBtn(title: 'Contact Us', link: ''),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ));
   }
 }
